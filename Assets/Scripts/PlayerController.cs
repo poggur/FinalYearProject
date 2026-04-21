033f41d22c2e81f0370f5a214abe4ac261ca59b0
Jack R <thereapercountdown@outlook.com>
2026-04-21 02:32:42 +0100

    added rolling
    rather unproductive today, dodge rolling now exists, next will be healing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 using UnityEngine.UI;
 using TMPro;
 using Unity.VisualScripting;
+using System.Collections;
 
 public class PlayerController : EntityClass
 {
@@ -10,10 +11,13 @@ public class PlayerController : EntityClass
     [SerializeField] private float walkSpeed = 10f;
     [SerializeField] private float sprintSpeed = 15f;
     [SerializeField] private int staminaDrain = 10;
+    [SerializeField] private float rollSpeed = 20f;
+    [SerializeField] private float rollTime = 0.25f;
 
     [Header("Input action references")]
     [SerializeField] private InputActionReference move;
     [SerializeField] private InputActionReference sprint;
+    [SerializeField] private InputActionReference roll;
 
     [Header("UI references")]
     [SerializeField] private Image healthBar;
@@ -25,6 +29,11 @@ public class PlayerController : EntityClass
     [SerializeField] private Camera mainCamera;
     [SerializeField] private CharacterController characterController;
     [SerializeField] private GameObject pivotPoint;
+
+    private bool rollCoroutineRunning = false;
+
+    private float moveSpeed;
+    private float startTime;
 
     //Variable that transforms get assigned to for the TransformDirection line in MovePlayer
     private Transform worldDirTransform;
@@ -48,7 +57,6 @@ public class PlayerController : EntityClass
 
     private void MovePlayer(Vector2 moveDirection)
     {
-        float moveSpeed;
         Vector3 moveForce;
 
         //Turn moveDirection into a vector3
@@ -87,16 +95,62 @@ public class PlayerController : EntityClass
         characterController.Move(moveForce * Time.deltaTime);
     }
 
+    //Damage
     private void OnTriggerStay(Collider other)
     {
         if(other.gameObject.layer == LayerMask.NameToLayer("Attack"))
         {
             int attackDamage = other.GetComponent<AttackScript>().attackDamage;
-            if (!healthCoroutineRunning)
+            if (!healthCoroutineRunning && !rollCoroutineRunning)
             {
                 StartCoroutine(ModifyHealth(attackDamage, false));
             }
         }
+    }
+
+    private IEnumerator SprintRollDecision()
+    {
+        yield return new WaitForSeconds(0.5f);
+        if (sprint.action.IsPressed())
+        {
+            moveSpeed = sprintSpeed;
+        }
+        else if (!sprint.action.IsPressed())
+        {
+            StartCoroutine(PlayerRoll());
+        }
+    }
+
+
+    private IEnumerator PlayerRoll()
+    {
+        rollCoroutineRunning = true;
+        Debug.Log(Time.time + " Regular time");
+        Debug.Log(startTime + rollTime+ " roll end time");
+
+        float whileTime = startTime;
+
+        //Turn moveDirection into a vector3
+        Vector3 moveDirection3 = new Vector3(moveDirection.x, 0, moveDirection.y);
+
+        //transform worldDirection from local direction to world direction
+        Vector3 worldDirection = worldDirTransform.TransformDirection(moveDirection3);
+
+        //Normalize
+        worldDirection = worldDirection.normalized;
+
+        Debug.Log(moveDirection);
+
+        while (whileTime <= startTime + rollTime) 
+        {
+            whileTime += Time.deltaTime;
+            
+            characterController.Move(rollSpeed * Time.deltaTime * worldDirection);
+            yield return new WaitForEndOfFrame();
+        }
+
+        StartCoroutine(ModifyStamina(20, false));
+        rollCoroutineRunning = false;
     }
 
     private void Start()
@@ -121,9 +175,15 @@ public class PlayerController : EntityClass
         }
 
         //Stamina
-        if (!sprint.action.IsPressed() && stamina < 100 && !staminaCoroutineRunning)
+        if (!sprint.action.IsPressed() && !staminaCoroutineRunning && !roll.action.IsPressed() && !rollCoroutineRunning && stamina < 100 )
         {
             StartCoroutine(ModifyStamina(staminaDrain * 2, true));
+        }
+
+        if (roll.action.IsPressed() && !rollCoroutineRunning && stamina > 20)
+        {
+            startTime = Time.time;
+            StartCoroutine(PlayerRoll());
         }
 
         //Update UI
